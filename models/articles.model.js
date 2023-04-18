800e36df21bba99207905930455262b94e8a67bd
Aadil <aadil@invigotouch.com>
2023-04-18 15:31:11 +0100

    added body

--- models/articles.model.js
+++ models/articles.model.js
@@ -4,7 +4,7 @@ exports.fetchArticles = () => {
   return db
     .query(
       `
-SELECT articles.article_id, articles.author, articles.title, articles.topic, articles.created_at, articles.votes, articles.article_img_url, CAST(COUNT(articles.article_id) AS INT) AS comment_count
+SELECT articles.article_id, articles.author, articles.title, articles.topic, articles.created_at, articles.votes, articles.article_img_url, articles.body, CAST(COUNT(articles.article_id) AS INT) AS comment_count
 FROM articles
 LEFT JOIN comments ON articles.article_id = comments.article_id
 GROUP BY articles.article_id
